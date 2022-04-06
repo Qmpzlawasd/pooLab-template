0ce451585029b9cd9e708b799083b246d9db9c2a
stefan <diaconustefan16@gmail.com>
2022-04-06 19:27:56 +0300

    acuma vine

--- main.cpp
+++ main.cpp
@@ -73,21 +73,18 @@ int main()
         }
         case 5:
 
-            //     for (size_t i = 0; i < judecatorie.size(); i++)
-            //     {
-            //         delete judecatorie[i];
-            //     }
-            //     exit(0);
-            //     break;
-            // default:
+                for (size_t i = 0; i < judecatorie.size(); i++)
+                {
+                    delete judecatorie[i];
+                }
+                exit(0);
+                break;
+            default:
 
-            //     for (size_t i = 0; i < judecatorie.size(); i++)
-            //     {
-            //         delete judecatorie[i];
-            //     }
-            exit(0);
-            break;
-        default:
+                for (size_t i = 0; i < judecatorie.size(); i++)
+                {
+                    delete judecatorie[i];
+                }
             exit(0);
             break;
         }
