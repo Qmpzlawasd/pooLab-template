063dc5f9c5663cec1833e75a0a408440e9904e79
stefan <diaconustefan16@gmail.com>
2022-04-06 19:31:30 +0300

    Bifa iei

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 int main()
 {
-    std::ifstream cin("date.txt");
+    // std::ifstream cin("date.txt");
     vector<Proces *> judecatorie;
     unsigned long n{4};
     int coma{-12};
